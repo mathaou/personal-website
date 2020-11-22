7e5be8ab0b3225097a53c346044787b8cda86c3a
Matthew Farstad <matthewwilliamfarstad@gmail.com>
2020-11-22 16:01:17 -0600

    asdfasdgg

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"golang.org/x/crypto/acme/autocert"
+
 	// "golang.org/x/crypto/acme/autocert"
 	"html/template"
 	"io"
@@ -20,16 +22,22 @@ func (t *TemplateRegistry) Render(w io.Writer, name string, data interface{}, c 
 }
 
 func main() {
+	debug := false
 	// Echo instance
 	e := echo.New()
-	// e.AutoTLSManager.HostPolicy = autocert.HostWhitelist("mfarstad.com")
-	e.Pre(middleware.HTTPSNonWWWRedirect())
+	e.Use(middleware.Gzip())
+	if debug {
+		e.Pre(middleware.NonWWWRedirect())
+	} else {
+		e.AutoTLSManager.HostPolicy = autocert.HostWhitelist("mfarstad.com")
+		e.Pre(middleware.HTTPSNonWWWRedirect())
+	}
 	// e.AutoTLSManager.Cache = autocert.DirCache("/var/www/.cache")
 	e.Use(middleware.Recover())
 	e.Use(middleware.Logger())
 
 	// Instantiate a template registry and register all html files inside the view folder
-	e.Renderer = &TemplateRegistry{
+	e.Renderer = &TemplateRegistry {
 		templates: template.Must(template.ParseGlob("static/*.html")),
 	}
 
@@ -37,9 +45,18 @@ func main() {
 
 	// Route => handler
 	e.GET("/", func(context echo.Context) error {
+		//context.Response().Header().Add("Content-Encoding", "gzip")
+		//context.Response().Header().Add("Vary", "Accept-Encoding")
 		return context.Render(http.StatusOK, "index.html", nil)
 	})
 
 	// Start the Echo server
-	e.Logger.Fatal(e.StartTLS(":443", "/etc/letsencrypt/live/mfarstad.com/cert.pem", "/etc/letsencrypt/live/mfarstad.com/privkey.pem"))
+	if debug {
+		e.Logger.Fatal(e.Start(":8080"))
+	} else {
+		go func() {
+			e.Logger.Fatal(e.Start(":80"))
+		}()
+		e.Logger.Fatal(e.StartTLS(":443", "/etc/letsencrypt/live/mfarstad.com/cert.pem", "/etc/letsencrypt/live/mfarstad.com/privkey.pem"))
+	}
 }
